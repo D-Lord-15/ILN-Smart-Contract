51a652a723a0fe286b0c1dad99fa5584e3edfdeb
Johnson Oyemade <johnsonoca@gmail.com>
2026-07-23 22:34:21 +0100

    feat(sdk): expose getReferralStats method to query referral counts by code

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -79,6 +79,7 @@ export {
 } from "./methods/insurance.js";
 export type { InsurancePoolInfo } from "@invoice-liquidity/types";
 export { getDistributionAccrual } from "./methods/distribution.js";
+export { getReferralStats } from "./methods/referralStats.js";
 export { TokenRegistry, tokenRegistry } from "./utils/tokenRegistry.js";
 export type { TokenInfo, NetworkName } from "./utils/tokenRegistry.js";
 export {
